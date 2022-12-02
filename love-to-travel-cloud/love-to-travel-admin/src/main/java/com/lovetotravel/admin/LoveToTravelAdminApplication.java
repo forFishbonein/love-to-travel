6cfb1dde95f3e4f99267728e9bd1b0b11d78c7dd
hyh315 <2856400640@qq.com>
2022-12-02 09:10:16 +0800

    创建管理端模块

---
+++ love-to-travel-cloud/love-to-travel-admin/src/main/java/com/lovetotravel/admin/LoveToTravelAdminApplication.java
@@ -0,0 +1,13 @@
+package com.lovetotravel.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveToTravelAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveToTravelAdminApplication.class, args);
+    }
+
+}
---
+++ love-to-travel-cloud/love-to-travel-admin/src/test/java/com/lovetotravel/admin/LoveToTravelAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovetotravel.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveToTravelAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
