487b0f2325729b60f83be2b95b88b04509410c8f
hyh315 <2856400640@qq.com>
2022-12-03 16:25:40 +0800

    勋章初始化

---
+++ love-to-travel-cloud/userservice/src/main/java/com/lovetotravel/user/service/impl/MedalServiceImpl.java
@@ -0,0 +1,14 @@
+package com.lovetotravel.user.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lovetotravel.user.entity.Medal;
+import com.lovetotravel.user.mapper.MedalMapper;
+import com.lovetotravel.user.service.MedalService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MedalServiceImpl extends ServiceImpl<MedalMapper, Medal> implements MedalService {
+
+
+
+}
