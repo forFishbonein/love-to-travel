a9aa94a927149c4cf29fe700add2cb67e94741bf
forFishbonein <1558637209@qq.com>
2022-12-03 21:27:49 +0800

    接口文档输出语句

--- love-to-travel-cloud/userservice/src/main/java/com/lovetotravel/user/UserserviceApplication.java
+++ love-to-travel-cloud/userservice/src/main/java/com/lovetotravel/user/UserserviceApplication.java
@@ -9,6 +9,7 @@ public class UserserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserserviceApplication.class, args);
         System.out.println("------userservice启动成功------");
+        System.out.println("------knife4j接口文档：http://localhost:8081/doc.html------");
     }
 
 }
