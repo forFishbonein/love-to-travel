90ecd37609f180074f75a58237aa99777354fed3
hyh315 <2856400640@qq.com>
2022-12-05 16:33:53 +0800

    用户id查询游记

--- love-to-travel-cloud/travelservice/src/main/java/com/lovetotravel/travel/service/NoteService.java
+++ love-to-travel-cloud/travelservice/src/main/java/com/lovetotravel/travel/service/NoteService.java
@@ -3,9 +3,13 @@ package com.lovetotravel.travel.service;
 import com.lovetotravel.travel.entity.Note;
 import com.lovetotravel.travel.entity.vo.NoteVo;
 
+import java.util.List;
+
 public interface NoteService {
 
     Note getById(String id);
+
+    List<Note> getByUserId(String userId);
 
     void insert(NoteVo noteVo);
 
