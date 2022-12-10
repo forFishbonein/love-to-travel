a472100214f7c45b28bf375a350dc741d150f564
hyh315 <2856400640@qq.com>
2022-12-10 16:59:51 +0800

    行程修复

--- love-to-travel-cloud/travelservice/src/main/java/com/lovetotravel/travel/service/impl/PlanServiceImpl.java
+++ love-to-travel-cloud/travelservice/src/main/java/com/lovetotravel/travel/service/impl/PlanServiceImpl.java
@@ -47,6 +47,8 @@ public class PlanServiceImpl implements PlanService {
         Update update = new Update();
 
         update.set("budget", plan.getBudget());
+        update.set("start", plan.getStart());
+        update.set("end", plan.getEnd());
 
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
@@ -61,6 +63,7 @@ public class PlanServiceImpl implements PlanService {
                 update.set("subPlans." + i + ".cityId", subPlans[i].getCityId());
                 update.set("subPlans." + i + ".city", subPlans[i].getCity());
                 update.set("subPlans." + i + ".dayLength", subPlans[i].getDayLength());
+                update.set("subPlans." + i + ".budget", subPlans[i].getBudget());
 
                 if (subPlans[i].getDays() != null) {
                     Days[] days = subPlans[i].getDays();
@@ -78,8 +81,6 @@ public class PlanServiceImpl implements PlanService {
                             for (int k = 0; k < routeLength; k++) {
                                 update.set("subPlans." + i + "days." + k + ".route." + k + ".origin", routes[j].getOrigin());
                                 update.set("subPlans." + i + "days." + k + ".route." + k + ".originName", routes[j].getOriginName());
-                                update.set("subPlans." + i + "days." + k + ".route." + k + ".destination", routes[j].getDestination());
-                                update.set("subPlans." + i + "days." + k + ".route." + k + ".destinationName", routes[j].getDestinationName());
                                 update.set("subPlans." + i + "days." + k + ".route." + k + ".departTime", routes[j].getDepartTime());
                                 update.set("subPlans." + i + "days." + k + ".route." + k + ".vehicle", routes[j].getVehicle());
                                 update.set("subPlans." + i + "days." + k + ".route." + k + ".weather", routes[j].getWeather());
