c904391d2c576cf917c48ffd831441684dbae0ee
hyh315 <2856400640@qq.com>
2022-12-11 17:23:53 +0800

    景区/游记用户浏览记录

--- love-to-travel-cloud/feign/src/main/java/com/lovetotravel/feign/clients/UserClient.java
+++ love-to-travel-cloud/feign/src/main/java/com/lovetotravel/feign/clients/UserClient.java
@@ -5,13 +5,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 @FeignClient(value = "userservice")
 public interface UserClient {
 
     @GetMapping("/user/{id}")
     User getById(@PathVariable("id") Long id);
 
-    @GetMapping("/user/{email}")
+    @GetMapping("/user/email/{email}")
     User getByEmail(@PathVariable("email") String email);
+
 
 }
