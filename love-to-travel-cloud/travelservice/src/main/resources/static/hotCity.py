7f418141c83cf646b559e5931a4f6018ead4d712
hyh315 <2856400640@qq.com>
2022-12-12 00:43:18 +0800

    python测试

--- love-to-travel-cloud/travelservice/src/main/resources/static/hotCity.py
+++ love-to-travel-cloud/travelservice/src/main/resources/static/hotCity.py
@@ -1,43 +1,43 @@
+print('0')
+
+import numpy as np
+a = np.arange(12).reshape(3,4)
+print(a)
+
+print('1')
+
+
+print('0.1')
 from pymongo import MongoClient
-import pandas as pd
 import pymysql
-#连接MongodB
+
+1
+print('1')
+
 def connectionDB():
     client = MongoClient('mongodb://admin:12345678@121.41.107.144:27017/travelservice?authMechanism=DEFAULT&authSource=admin&authMechanism=SCRAM-SHA-1')
-    ##指定要操作的数据库，test
     db = client.travelservice
-    ##限定数据库表，plan
     mycol = db["note"]
-
-    #获取SourceCode，直接从MongoDB查询获得
     City = mycol.distinct( "city")
     Date = mycol.distinct("createTime")
-    print(City)
-    print(Date)
     return City,Date
-
+print('2')
 def GetHotCity ():
     list_city,list_date = connectionDB()
     list_city_new =[]
     for i in range(len(list_city)):
-        if int(list_date[i][:4]) > 2016:
-            #         print(int(data['date'][i][:4]))
-            list_city_new.append(list_city[i])
-    list_city_new
-    counts = {}         #计数器
+        list_city_new.append(list_city[i])
+    counts ={}
     for word in list_city_new:
         counts[word] = counts.get(word, 0) + 1
-
-    counts = sorted(counts.items(), key = lambda x: x[1], reverse = True)
-
+    counts = sorted(counts.items(), key = lambda x: x[1], reverse = True)[:10]
     list_res=[]
-    for i in range(10):
-        word, count = counts[i]
+
+    for i in range(len(counts)):
+        word,count = counts[i]
         list_res.append(word)
     return list_res
-#     print(list_res)
-
-
+print('3')
 def get_conn():
     return pymysql.connect(
         host='47.98.138.0',
@@ -47,35 +47,32 @@ def get_conn():
         database='travelservice',
         charset='utf8'
     )
-
+print('4')
 def HotCityData():
     conn = get_conn()
     sql = "select * from city"
+    list1=[]
+    cursor = conn.cursor()
     try:
-        cursor = conn.cursor()
         cursor.execute(sql)
         list1=list(cursor.fetchall())
     except pymysql.Error as e:
         print('连接失败')
 
     list_res=GetHotCity ()
-    #     list_resf=[]
-    #     print(list1)
-    #     list1=list(query_data(sql))
+
     for i in list_res:
         i=i+'市'
         for k in list1:
             if i==k[1]:
-                #                 list_resf.append(k)
-                sqlInsert="insert hot_city value(%s,%s,%s,%s,%s,%s)"
-                value = k
+                sqlInsert="insert hot_city value(%s,%s,%s,%s)"
+                value = [k[0],k[1],k[3],k[4]]
                 try:
                     cursor.execute(sqlInsert,value)
                     conn.commit()
                 finally:
                     conn.close
 
-#     return list_resf
-
-if __name__ == "__main__":
-    HotCityData()
+print('123123')
+HotCityData()
+print('1231233333')
