f8857ded5b63c3f992bdfd6a9e849f2c8fd2386d
forFishbonein <1558637209@qq.com>
2022-12-13 00:44:33 +0800

    前段去旅游页面8

--- love-to-travel-cloud/travelservice/src/main/java/com/lovetotravel/travel/mapper/SceneryCommentMapper.java
+++ love-to-travel-cloud/travelservice/src/main/java/com/lovetotravel/travel/mapper/SceneryCommentMapper.java
@@ -4,6 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lovetotravel.travel.entity.SceneryComment;
 import lombok.Data;
 
-@Data
+//@Data
 public interface SceneryCommentMapper extends BaseMapper<SceneryComment> {
 }
