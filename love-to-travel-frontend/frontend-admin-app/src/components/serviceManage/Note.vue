f7587533a627a5f2c79935bb7decae3434533b15
小小小小小 <1043643721@qq.com>
2022-12-12 15:24:17 +0800

    后台游记

--- love-to-travel-frontend/frontend-admin-app/src/components/serviceManage/Note.vue
+++ love-to-travel-frontend/frontend-admin-app/src/components/serviceManage/Note.vue
@@ -1,3 +1,5 @@
+<script setup lang="ts"></script>
+
 <template>
   <el-card class="box-card">
     <template #header>
@@ -11,174 +13,112 @@
           <el-button class="button" type="warning" round @click="multiDelete">删除</el-button>
         </div>
 
+
+
       </div>
     </template>
     <el-table :data="tableData" style="width: 100%" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" />
 
-      <el-table-column prop="id.date" label="游记时间" width="100" />
-      <el-table-column prop="id.timestamp" label="游记时间戳" width="100" />
-      <el-table-column prop="title" label="游记标题" width="100" />
-      <el-table-column prop="userId" label="用户ID" width="100" />
-      <el-table-column prop="city" label="所属城市" width="150" />
-      <el-table-column prop="content" label="游记内容" width="100" />
-      <el-table-column prop="url" label="图片地址" width="100" />
-      <el-table-column prop="comment" label="评论量" width="100" />
-      <el-table-column prop="like" label="点赞量" width="100" />
-      <el-table-column prop="view" label="浏览量" width="100" />
-      <el-table-column prop="trip" label="标签" width="100" />
-      <el-table-column prop="planId" label="行程" width="100" />
-      <el-table-column fixed="right" property="id" label="Operations" width="120">
+      <el-table-column prop="data.id" label="游记id" width="100" />
+      <el-table-column prop="data.title" label="游记标题" width="100" />
+      <el-table-column prop="data.userId" label="用户ID" width="100" />
+      <el-table-column prop="data.city" label="所属城市" width="150" />
+      <el-table-column prop="data.content" label="游记内容" width="100" />
+      <el-table-column prop="data.url" label="图片地址" width="100" />
+      <el-table-column prop="data.comment" label="评论量" width="100" />
+      <el-table-column prop="data.like" label="点赞量" width="100" />
+      <el-table-column prop="data.view" label="浏览量" width="100" />
+      <el-table-column prop="data.trip" label="标签" width="100" />
+      <el-table-column prop="data.planId" label="行程" width="100" />
+      <el-table-column fixed="right" label="Operations" width="120">
         <template #default>
-          <el-button link type="primary" size="small" @click="singleDelete(id)">删除</el-button
+          <el-button link type="primary" size="small" @click="singleDelete">删除</el-button
           >
           <el-button link type="primary" size="small" @click="openUpdateDialog">编辑</el-button>
         </template>
       </el-table-column>
     </el-table>
   </el-card>
-  <!-- 对话框：添加修改功能 -->
-  <el-dialog v-model="dialogFormVisible" :title="title">
-    <el-form :model="form">
-      <el-form-item label="游记时间" :label-width="formLabelWidth">
-        <el-input v-model="form.id.date" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="游记时间戳" :label-width="formLabelWidth">
-        <el-input v-model="form.id.timestamp" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="游记标题" :label-width="formLabelWidth">
-        <el-input v-model="form.title" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="用户ID" :label-width="formLabelWidth">
-        <el-input v-model="form.userId" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="所属城市" :label-width="formLabelWidth">
-        <el-input v-model="form.city" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="游记内容" :label-width="formLabelWidth">
-        <el-input v-model="form.content" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="图片地址" :label-width="formLabelWidth">
-        <el-input v-model="form.url" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="评论量" :label-width="formLabelWidth">
-        <el-input v-model="form.comment" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="点赞量" :label-width="formLabelWidth">
-        <el-input v-model="form.like" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="浏览量" :label-width="formLabelWidth">
-        <el-input v-model="form.view" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="标签" :label-width="formLabelWidth">
-        <el-input v-model="form.trip" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="行程" :label-width="formLabelWidth">
-        <el-input v-model="form.planId" autocomplete="off" />
-      </el-form-item>
-
-    </el-form>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取消</el-button>
-        <el-button type="primary" @click="btnAddUpdate">
-<!--          {{btnName}}-->
-        </el-button>
-      </span>
-    </template>
-  </el-dialog>
-
-
 </template>
 
-
-<script  lang="ts">
-import {getNote} from "../../apis/userManage/note.js";
-// import {ElMessage} from 'element-plus';
+<script>
+import {ElMessage} from 'element-plus'
 export default {
   data(){
     return{
       dialogFormVisible:false, //对话框是否显示
       queryStr:"",  //查询条件
       multipleSelection:[], //多选删除
-      tableData: [], //游记信息数据
+      tableData: [], //学生信息数据
       form:{},   //对话框表单数据
       formLabelWidth:"140px",  //对话框label宽度
       title:"",  //对话框标题
       btnName:""  //对话框按钮文字
     }
-  },
-  mounted(){
-    getNote().then((response) => {
-      var _this = this;
-      console.log(response.data);
-      _this.tableData = response.data;
-
-    })
-  },
-
-
-  methods:{
-    openAddDialog(){
-      this.btnName = "添加"
-      this.title = "添加商品信息"
-      this.dialogFormVisible = true
-      console.log("openAddDialog")
-    },
-    openUpdateDialog(){
-      this.btnName = "修改"
-      this.title = "修改商品信息"
-      this.dialogFormVisible = true
-      console.log("openUpdateDialog")
-    },
-    //   addShop(){
-    //     var _this = this;
-    //     //this.form.stu_interest = this.form.stu_interest.join(',')
-    //     this.$http.post("/shop/shops",this.form).then(function(response){
-    //       console.log(response.data);
-    //       if(response.data==1){
-    //         ElMessage({
-    //           message: '商品信息添加成功',
-    //           type: 'success',
-    //         })
-    //       }else {
-    //         ElMessage({
-    //           message: '商品信息添加失败',
-    //           type: 'warning',
-    //         })
-    //       }
-    //     })
-    //   },
-    //
-    //   //.......
-    btnAddUpdate(){
-      if(this.btnName=="修改"){
-
-        console.log("修改。。。")
-
-      }
-      if(this.btnName=="添加"){
-        //this.addShop()
-
-        console.log("添加。。。")
-        console.log(this.form)
-      }
-      this.dialogFormVisible = false
-    },
-    singleDelete(id){
-      console.log(id)
-    },
-    multiDelete(){
-      console.log("multiDelete()")
-    },
-    queryInfo(){
-      console.log("queryInfo...");
-    },
-    handleSelectionChange(val){
-      this.multipleSelection = val
-      console.log(this.multipleSelection)
-    }
-  },
+  }
+  //
+  // methods:{
+  //   openAddDialog(){
+  //     this.btnName = "添加"
+  //     this.title = "添加商品信息"
+  //     this.dialogFormVisible = true
+  //     console.log("openAddDialog")
+  //   },
+  //   openUpdateDialog(){
+  //     this.btnName = "修改"
+  //     this.title = "修改商品信息"
+  //     this.dialogFormVisible = true
+  //     console.log("openUpdateDialog")
+  //   },
+  //   addShop(){
+  //     var _this = this;
+  //     //this.form.stu_interest = this.form.stu_interest.join(',')
+  //     this.$http.post("/shop/shops",this.form).then(function(response){
+  //       console.log(response.data);
+  //       if(response.data==1){
+  //         ElMessage({
+  //           message: '商品信息添加成功',
+  //           type: 'success',
+  //         })
+  //       }else {
+  //         ElMessage({
+  //           message: '商品信息添加失败',
+  //           type: 'warning',
+  //         })
+  //       }
+  //     })
+  //   },
+  //
+  //   //.......
+  //   btnAddUpdate(){
+  //     if(this.btnName=="修改"){
+  //
+  //       console.log("修改。。。")
+  //
+  //     }
+  //     if(this.btnName=="添加"){
+  //       this.addShop()
+  //
+  //       console.log("添加。。。")
+  //       console.log(this.form)
+  //     }
+  //     this.dialogFormVisible = false
+  //   },
+  //   singleDelete(){
+  //     console.log("singleDelete()")
+  //   },
+  //   multiDelete(){
+  //     console.log("multiDelete()")
+  //   },
+  //   queryInfo(){
+  //     console.log("queryInfo...");
+  //   },
+  //   handleSelectionChange(val){
+  //     this.multipleSelection = val
+  //     console.log(this.multipleSelection)
+  //   }
+  // },
   // mounted(){
   //   var _this = this;
   //   this.$http.get("/shop/shops").then(function(response){
