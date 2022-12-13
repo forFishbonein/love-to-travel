7d9734640e5f2e77efdae2cc9485a28d2eb30d7c
hyh315 <2856400640@qq.com>
2022-12-13 22:00:20 +0800

    全局查询

--- love-to-travel-cloud/travelservice/src/main/java/com/lovetotravel/travel/entity/vo/GlobalQuery.java
+++ love-to-travel-cloud/travelservice/src/main/java/com/lovetotravel/travel/entity/vo/GlobalQuery.java
@@ -1,12 +1,23 @@
 package com.lovetotravel.travel.entity.vo;
 
+import com.lovetotravel.travel.entity.City;
+import com.lovetotravel.travel.entity.Note;
+import com.lovetotravel.travel.entity.Scenery;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class GlobalQuery {
 
+    @ApiModelProperty("游记列表")
+    private List<Note> noteList;
 
+    @ApiModelProperty("城市列表")
+    private List<City> cityList;
 
-
+    @ApiModelProperty("景点列表")
+    private List<Scenery> sceneryList;
 
 }
