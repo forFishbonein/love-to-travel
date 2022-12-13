06c44594b04838090b2e9ba54d30c75870fb7efb
hyh315 <2856400640@qq.com>
2022-12-13 11:09:37 +0800

    足迹 想去、去过删除

--- love-to-travel-cloud/travelservice/src/main/java/com/lovetotravel/travel/service/impl/FootpointServiceImpl.java
+++ love-to-travel-cloud/travelservice/src/main/java/com/lovetotravel/travel/service/impl/FootpointServiceImpl.java
@@ -136,16 +136,22 @@ public class FootpointServiceImpl implements FootpointService {
         Query query = new Query();
         query.addCriteria(Criteria.where("userId").is(wantRemoveVo.getUserId()));
         Footpoint footpointInMongo = mongoTemplate.findOne(query, Footpoint.class);
-        Footpoint footpoint = footpointInMongo;
-        System.out.println(footpoint);
-        List<Want> wants = footpoint.getWants();
-        Want want = new Want();
-        want.setCityId(wantRemoveVo.getCityId());
-        want.setCreateTime(wantRemoveVo.getCreateTime());
-        wants.remove(want);
-        footpoint.setWants(wants);
-        mongoTemplate.remove(query, Footpoint.class);
-        mongoTemplate.insert(footpoint);
+        Footpoint footpoint;
+        List<Want> wants;
+        if (footpointInMongo != null) {
+            footpoint = footpointInMongo;
+            System.out.println(footpoint);
+            wants = footpoint.getWants();
+            if (wants != null) {
+                Want want = new Want();
+                want.setCityId(wantRemoveVo.getCityId());
+                want.setCreateTime(wantRemoveVo.getCreateTime());
+                wants.remove(want);
+                footpoint.setWants(wants);
+                mongoTemplate.remove(query, Footpoint.class);
+                mongoTemplate.insert(footpoint);
+            }
+        }
     }
 
     @Override
@@ -153,15 +159,21 @@ public class FootpointServiceImpl implements FootpointService {
         Query query = new Query();
         query.addCriteria(Criteria.where("userId").is(beenRemoveVo.getUserId()));
         Footpoint footpointInMongo = mongoTemplate.findOne(query, Footpoint.class);
-        Footpoint footpoint = footpointInMongo;
-        List<Been> beens = footpoint.getBeens();
-        Been been = new Been();
-        been.setCityId(beenRemoveVo.getCityId());
-        been.setCreateTime(beenRemoveVo.getCreateTime());
-        been.setScore(beenRemoveVo.getScore());
-        beens.remove(been);
-        footpoint.setBeens(beens);
-        mongoTemplate.remove(query, Footpoint.class);
-        mongoTemplate.insert(footpoint);
+        Footpoint footpoint;
+        List<Been> beens;
+        if (footpointInMongo != null) {
+            footpoint = footpointInMongo;
+            System.out.println(footpoint);
+            beens = footpoint.getBeens();
+            if (beens != null) {
+                Been been = new Been();
+                been.setCityId(beenRemoveVo.getCityId());
+                been.setCreateTime(beenRemoveVo.getCreateTime());
+                beens.remove(been);
+                footpoint.setBeens(beens);
+                mongoTemplate.remove(query, Footpoint.class);
+                mongoTemplate.insert(footpoint);
+            }
+        }
     }
 }
