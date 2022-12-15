cf7650cdf5b7d3447ace797de17a277a04b11d6c
hyh315 <2856400640@qq.com>
2022-12-15 13:16:46 +0800

    文件上传接口

---
+++ love-to-travel-cloud/travelservice/src/main/java/com/lovetotravel/travel/controller/FileUploadController.java
@@ -0,0 +1,29 @@
+package com.lovetotravel.travel.controller;
+
+
+import com.lovetotravel.travel.entity.vo.PicUploadResult;
+import com.lovetotravel.travel.result.Result;
+import com.lovetotravel.travel.service.FileUploadService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/file")
+@Slf4j
+public class FileUploadController {
+
+    final FileUploadService fileUploadService;
+
+    public FileUploadController(FileUploadService fileUploadService) {
+        this.fileUploadService = fileUploadService;
+    }
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public Result<PicUploadResult> upload(@RequestParam("file") MultipartFile multipartFile) {
+        return Result.success(fileUploadService.upload(multipartFile));
+    }
+
+
+}
