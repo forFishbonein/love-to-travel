548f7cb8071f98e7e3a0456562a5bb99fc9814aa
hyh315 <2856400640@qq.com>
2022-12-18 20:32:03 +0800

    neo4j

--- love-to-travel-frontend/frontend-main-app/src/views/canvasTravel/CanvasTravel.vue
+++ love-to-travel-frontend/frontend-main-app/src/views/canvasTravel/CanvasTravel.vue
@@ -362,19 +362,21 @@ export default {
   },
 
   methods: {
-    getGraphData(){
-      var _this = this
-      this.axios.get("canvas/"+'炽金古城')
-          //   this.axios.get("person/all")
-          .then(function (response) {
-            console.log(response)
-            // _this.testGraph["nodes"] = [response.data]
-            // _this.initGraph(_this.testGraph)
-          })
-          .catch(function (error) {
-            console.log(error)
-          })
-    },
+    // getGraphData(){
+    //   var _this = this
+    //   this.axios.get("canvas/"+'炽金古城')
+    //       //   this.axios.get("person/all")
+    //       .then(function (response) {
+    //         console.log(response)
+    //         // _this.testGraph["nodes"] = [response.data]
+    //         // _this.initGraph(_this.testGraph)
+    //       })
+    //       .catch(function (error) {
+    //         console.log(error)
+    //       })
+    // },
+
+
     initGraph(data) {
       var _this = this
       const links = data.links.map(d => Object.create(d))
