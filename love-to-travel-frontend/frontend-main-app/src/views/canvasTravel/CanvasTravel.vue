6a94fd1e63954b394ec9650e82cd82e6bdf05f43
hyh315 <2856400640@qq.com>
2022-12-18 19:47:38 +0800

    neo4j

--- love-to-travel-frontend/frontend-main-app/src/views/canvasTravel/CanvasTravel.vue
+++ love-to-travel-frontend/frontend-main-app/src/views/canvasTravel/CanvasTravel.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+
+  </div>
 </template>
 <script>
 import * as d3 from 'd3'
 
-
 export default {
+  name: "Canvas",
+
   data() {
     return {
       width: 600,
