f0e913548395f5323298bc85ab4bc3b57362579d
forFishbonein <1558637209@qq.com>
2022-12-18 18:26:59 +0800

    景区评论评分

--- love-to-travel-frontend/frontend-main-app/src/views/goTravel/scenery/SceneryDetail.vue
+++ love-to-travel-frontend/frontend-main-app/src/views/goTravel/scenery/SceneryDetail.vue
@@ -3,6 +3,9 @@ import { ref } from "vue";
 import { getOneSceneryInfoById } from "@/apis/travelService/scenery";
 import { theCityScenerysInfoType } from "@/apis/interface/myInterface";
 import { keepTwoDecimal } from "@/utils/filters/number";
+import { mainStore } from "@/store/user";
+import { sceneryCommentParams } from "@/apis/travelService/tInterface";
+const store = mainStore();
 const props = defineProps<{
   sceneryId: number;
 }>();
@@ -33,6 +36,81 @@ const requestOneSceneryInfo = async () => {
     });
 };
 requestOneSceneryInfo();
+
+const commentContent = ref("");
+const addFirstComment = async () => {
+  // alert(pId);
+  if (store.userInfo.id) {
+    const firstCommentParams: sceneryCommentParams = {
+      parentId: "",
+      content: firstCommentContent.value,
+      userId: store.userInfo.id,
+      // @ts-ignore
+      userName: store.userInfo.name,
+      noteId: noteId,
+    };
+    console.log("============");
+    console.log(firstCommentParams);
+    console.log("============");
+    await addAComment(firstCommentParams)
+      .then((res: any) => {
+        if (res.code != 0) {
+          //@ts-ignore
+          ElMessage({
+            type: "error",
+            message: res.msg,
+          });
+        } else {
+          //@ts-ignore
+          ElMessage({
+            type: "success",
+            message: "评论成功",
+          });
+          setTimeout(
+            () =>
+              //更新评论
+              getCommentsByNoteId(noteId)
+                .then((res: any) => {
+                  if (res.code != 0) {
+                    //@ts-ignore
+                    ElMessage({
+                      type: "error",
+                      message: res.msg,
+                    });
+                  } else {
+                    //这里必须要清空一下
+                    noteCommentsInfo.value = [] as theNoteComment[];
+                    finalCommentsArray.value = [] as tranformComments[];
+                    noteCommentsInfo.value = res.data;
+                    commentsFormat(noteCommentsInfo.value);
+                  }
+                })
+                .catch((error) => {
+                  //@ts-ignore
+                  ElMessage({
+                    type: "error",
+                    message: error.message,
+                  });
+                }),
+            1000
+          );
+        }
+      })
+      .catch((error) => {
+        //@ts-ignore
+        ElMessage({
+          type: "error",
+          message: error.message,
+        });
+      });
+  } else {
+    //@ts-ignore
+    ElMessage({
+      type: "warning",
+      message: "请先登录！",
+    });
+  }
+};
 </script>
 <script lang="ts">
 (function ($) {
@@ -99,7 +177,7 @@ requestOneSceneryInfo();
                     <p>景区评分</p>
                   </div>
                   <div class="destinations-details__overview-right">
-                    <p>{{ sceneryInfo.score }}</p>
+                    <p>{{ sceneryInfo.score || "-" }}</p>
                   </div>
                 </li>
                 <li>
@@ -115,7 +193,7 @@ requestOneSceneryInfo();
                     <p>景区地址</p>
                   </div>
                   <div class="destinations-details__overview-right">
-                    <p>{{ sceneryInfo.address }}</p>
+                    <p>{{ sceneryInfo.address || "-" }}</p>
                   </div>
                 </li>
                 <li>
@@ -123,7 +201,7 @@ requestOneSceneryInfo();
                     <p>景区电话</p>
                   </div>
                   <div class="destinations-details__overview-right">
-                    <p>{{ sceneryInfo.tele }}</p>
+                    <p>{{ sceneryInfo.tele || "-" }}</p>
                   </div>
                 </li>
                 <li>
@@ -185,6 +263,27 @@ requestOneSceneryInfo();
                 </div>
               </div>
             </div>
+            <div class="comment-form">
+              <h3 class="comment-form__title">发表评论</h3>
+              <div class="row">
+                <div class="col-xl-12">
+                  <div class="comment-form__input-box">
+                    <textarea
+                      name="message"
+                      placeholder="写下评论内容"
+                      v-model="commentContent"
+                    ></textarea>
+                    <button
+                      type="submit"
+                      class="thm-btn comment-form__btn"
+                      @click="addComment"
+                    >
+                      发表
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div class="col-xl-4 col-lg-5">
@@ -233,12 +332,63 @@ requestOneSceneryInfo();
                   <el-icon :color="`#e8604c`"><Star /></el-icon
                   >{{ sceneryInfo.score }}
                 </h3>
-                <h4>
+                <h4 style="font-size: 18px">
                   {{ sceneryInfo.name }}<br />
                   ¥{{ sceneryInfo.ticket }}
                 </h4>
               </div>
             </div>
+            <!-- <div class="sidebar__single sidebar__category">
+              <h3 class="sidebar__title">游记评论</h3>
+              <ul class="sidebar__category-list list-unstyled">
+                <li>
+                  <el-scrollbar max-height="800px" style="padding-right: 10px">
+                    <div
+                      :name="index"
+                      v-for="(item, index) in finalCommentsArray"
+                      :key="index"
+                    >
+                      <div class="comment-container">
+                        <div class="comment-header">
+                          <p>
+                            <router-link to="">{{ item.userName }}</router-link>
+                          </p>
+                          <p>
+                            {{ item.createTime }}
+                          </p>
+                        </div>
+                        <div class="comment-body">
+                          <el-scrollbar max-height="40px">
+                            <div class="content-main">
+                              {{ item.content }}
+                            </div>
+                          </el-scrollbar>
+                        </div>
+                      </div>
+                      <el-scrollbar max-height="400px">
+                        <ul class="second-ul">
+                          <li
+                            v-for="(i, index2) in item.son"
+                            :key="index2"
+                            class="second-item"
+                          >
+                            <p>
+                              <span style="color: #e8604c">{{
+                                i.userName
+                              }}</span
+                              ><span style="color: #909399">{{
+                                i.createTime
+                              }}</span>
+                            </p>
+                            <p>{{ i.content }}</p>
+                          </li>
+                        </ul>
+                      </el-scrollbar>
+                    </div>
+                  </el-scrollbar>
+                </li>
+              </ul>
+            </div> -->
           </div>
         </div>
       </div>
