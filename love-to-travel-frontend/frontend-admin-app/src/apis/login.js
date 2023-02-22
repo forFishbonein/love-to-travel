fbcdb3ba07e23342ff5bc683a7e53ada8116dbd8
hyh315 <2856400640@qq.com>
2023-02-23 01:20:20 +0800

    admin

--- love-to-travel-frontend/frontend-admin-app/src/apis/login.js
+++ love-to-travel-frontend/frontend-admin-app/src/apis/login.js
@@ -3,7 +3,7 @@ import httpRequest from "@/request";
 export const login = (data) => {
     return httpRequest({
         method: "post",
-        url: "http://localhost:8080/user/login",
+        url: "http://localhost:8899/user/login",
         data,
         loading: true, //显示加载圈
     });
