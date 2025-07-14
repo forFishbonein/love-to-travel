f47faf31897367124ce9e89c0f4288ef4eb34b13
Wenhai Hao <102403679+forFishbonein@users.noreply.github.com>
2025-07-14 18:44:10 +0800

    Update oss.ts

--- love-to-travel-frontend/frontend-main-app/src/utils/oss.ts
+++ love-to-travel-frontend/frontend-main-app/src/utils/oss.ts
@@ -2,7 +2,7 @@
 import OSS from "ali-oss";
 export let client = new OSS({
   region: "oss-cn-hangzhou",
-  accessKeyId: "LTAI5tRM3FjZEZjXbNoTAUEu",
-  accessKeySecret: "6xyVEBQPktBIYxqRE7T77rcg1zFNdW",
+  accessKeyId: "xxx",
+  accessKeySecret: "xxx",
   bucket: "noteimg123",
 });
